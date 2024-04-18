7ef2ca81508d8157282f7248e4a333e1de55f3de
nguyenanhvu03 <nguyenanhvu.tdc2223@gmail.com>
2024-04-18 16:08:44 +0700

    commit message

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,16 @@
 import express from "express";
+import homeController from '../controllers/homeController';
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello world')
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+
+    router.get('/abc', (req, res) => {
+        return res.send('hello world check abc')
     });
+
     return app.use("/", router);
 }
 
