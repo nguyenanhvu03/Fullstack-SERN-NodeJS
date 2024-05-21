329184192b3541f2ebbc9e1342330cd532e8a885
nguyenanhvu03 <nguyenanhvu.tdc2223@gmail.com>
2024-05-21 15:31:40 +0700

    commit message

--- src/services/userService.js
+++ src/services/userService.js
@@ -168,7 +168,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.id) {
+            if (!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: 'Missing required parameters'
@@ -182,6 +182,10 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phonenumber = data.phonenumber;
 
                 await user.save();
                 // await db.User.save({
